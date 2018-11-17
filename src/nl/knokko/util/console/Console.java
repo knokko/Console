881703aefda4bc7ff2adeee635f15d8f207e6e21
knokko <knokogator@hotmail.com>
2018-11-17 17:49:49 +0100

    Fix license headers and indentation

--- src/nl/knokko/util/console/Console.java
+++ src/nl/knokko/util/console/Console.java
@@ -1,15 +1,15 @@
-/* 
+/*******************************************************************************
  * The MIT License
  *
- * Copyright 2018 20182191.
+ * Copyright (c) 2018 knokko
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
+ *  of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ *  
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
@@ -20,7 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- */
+ *******************************************************************************/
 package nl.knokko.util.console;
 
 import java.io.BufferedReader;
@@ -29,28 +29,28 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 
 public abstract class Console implements Runnable {
-	
+
 	private final PrintStream out;
 	private final BufferedReader in;
-	
+
 	protected boolean stopping;
-	
-	public Console(){
+
+	public Console() {
 		this(System.out, new BufferedReader(new InputStreamReader(System.in)));
 	}
-	
-	public Console(PrintStream out, BufferedReader in){
+
+	public Console(PrintStream out, BufferedReader in) {
 		this.out = out;
 		this.in = in;
 	}
-	
+
 	@Override
 	public void run() {
 		try {
 			println("Starting console");
-			while(!stopping){
+			while (!stopping) {
 				String command = in.readLine();
-				if(command != null)
+				if (command != null)
 					executeCommand(command.split(" "));
 				else {
 					println("End of console reached");
@@ -59,22 +59,22 @@ public abstract class Console implements Runnable {
 					return;
 				}
 			}
-		} catch(IOException ex){
+		} catch (IOException ex) {
 			println("Console IO exception occured: " + ex.getMessage());
 			stop();
 		}
 	}
-	
+
 	protected abstract void stop();
-	
+
 	protected abstract void executeCommand(String[] command);
-	
-	public PrintStream getWriter(){
+
+	public PrintStream getWriter() {
 		return out;
 	}
-	
-	public void println(String message){
-		if(!stopping)
+
+	public void println(String message) {
+		if (!stopping)
 			out.println(message);
 	}
 }
